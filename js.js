129862ceb32cc60e2f621a1e1af6e559aada7b8e
Ernest Muzafarov <108494517+MrSSStrange@users.noreply.github.com>
2026-05-03 19:33:12 +0300

    Update js.js

--- js.js
+++ js.js
@@ -14,11 +14,18 @@ button.addEventListener("click", function () {
     }
 
     if (firstName.match(/[0-9]/) || secondName.match(/[0-9]/)) {
-        result.textContent = "Имена не должны содержать цыфры";
+        result.textContent = "Имена не должны содержать цифры";
         return;
 
     }
 
     const randomNumber = Math.floor(Math.random() * 100);
-    result.textContent = `Результат совместимости имен ${firstName} и ${secondName} равен ${randomNumber}`
-});
+
+    if (randomNumber <= 33) {
+        result.textContent = `Результат совместимости имен ${firstName} и ${secondName} равен ${randomNumber}. Вам нужно поискать человека с другим именем 😔`
+    } else if (randomNumber > 33 && randomNumber <= 66) {
+        result.textContent = `Результат совместимости имен ${firstName} и ${secondName} равен ${randomNumber}. Совместимость неплохая, но можно лучше 🙂`
+    } else {
+        result.textContent = `Результат совместимости имен ${firstName} и ${secondName} равен ${randomNumber}. Вы идеально подходите друг к другу 🥹`
+    }
+    });
